e990b9546d6eeed2f5142e5228c8af52010d837c
Deonnn <41499793+D30N@users.noreply.github.com>
2019-09-21 15:26:04 +0530

    Updated NO_LOAD

--- sample_config.py
+++ sample_config.py
@@ -56,8 +56,6 @@ class Config(object):
     # foloowing plugins won't work on Heroku,
     # because of their ephemeral file system
     NO_LOAD = [
-        "fwd",
-        "telegraph",
         "gban"
     ]
     # Get your own API key from https://www.remove.bg/ or
